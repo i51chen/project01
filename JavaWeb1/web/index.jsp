30a163baa631d4cd8a90ca541114f804bcf903eb
i51chen <56567729+i51chen@users.noreply.github.com>
2020-02-07 06:38:27 +0800

    Initial commit

---
+++ JavaWeb1/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wyc
+  Date: 2020/2/7
+  Time: 4:35
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>首页</title>
+  </head>
+  <body>
+  <h1 style="color: #111111;">h1标题</h1>
+  </body>
+</html>
---
+++ src/com/wyc/java/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.wyc.java;
+
+public class HelloWorld {
+    public static void main(String args[]) {
+        main2(2);
+        System.out.println("啊可是对方老酒馆换");
+    }
+
+    public static void main2(int a) {
+        //sout soutp soutm soutv xxx.sout
+        System.out.println("HelloWorld.main2");
+        System.out.println("a = [" + a + "]");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("i = " + i);
+        }
+        String [] strings = new String[]{"1","2","3"};
+        System.out.println(strings[2]);
+
+        //单行注释
+
+    }
+}
